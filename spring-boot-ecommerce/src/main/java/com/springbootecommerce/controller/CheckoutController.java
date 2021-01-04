34841e8ca0a4355ab097dd83cabf2d23c28a1d6d
OuniAbir <ouniabir118@gmail.com>
2021-01-04 14:05:01 +0100

    CheckoutForm - save the order to DB from backend : develop the checkout controller post RestAPI

---
+++ spring-boot-ecommerce/src/main/java/com/springbootecommerce/controller/CheckoutController.java
@@ -0,0 +1,33 @@
+package com.springbootecommerce.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springbootecommerce.dto.Purchase;
+import com.springbootecommerce.dto.PurchaseResponse;
+import com.springbootecommerce.service.CheckoutService;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/checkout")
+public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
+
+}
+
